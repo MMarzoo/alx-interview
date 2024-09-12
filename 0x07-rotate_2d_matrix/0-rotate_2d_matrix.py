89237b4f71ab672e79096c00e7a91a34afaf751b
MMarzoo <mmarzouk12378@gmail.com>
2024-09-12 20:36:57 +0300

    adding files

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,11 +12,7 @@ def rotate_2d_matrix(matrix):
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = matrix[i][j]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     # Step 2: Reverse each row to complete the 90-degree clockwise rotation
-    for i in range(n):
-        matrix[i].reverse()
-
     for row in matrix:
-        print(row)
+        row.reverse()
