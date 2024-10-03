6937cccf87a342bb71f9c0a0c0e38da5d88518f5
MMarzoo <mmarzouk12378@gmail.com>
2024-10-03 18:20:28 +0300

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,61 +1,49 @@
 #!/usr/bin/python3
-"""
-Prime_Game
-"""
-
-
-def SieveOfEratosthenes(n):
-    """Returns a list of prime numbers up to
-    n using the Sieve of Eratosthenes."""
-    primes = [True] * (n + 1)
-    primes[0], primes[1] = False, False
-
-    p = 2
-    while (p * p <= n):
-        if primes[p]:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-
-    return [i for i in range(n + 1) if primes[i]]
+""" Who would win the prime game """
 
 
 def isWinner(x, nums):
-    """Determines who wins the most rounds between Maria and Ben."""
-    if not nums or x <= 0:
+    """ Let's find out """
+
+    # Case of no game
+    if x <= 0 or nums is None:
         return None
 
-    max_n = max(nums)
-    primes = SieveOfEratosthenes(max_n)
-    prime_set = set(primes)
-
+    # Set the scores
     maria_wins = 0
     ben_wins = 0
 
+    # Create a list with size of the highest number given
+    # and fill it with 1, where  if the number is 1
+    # then it is still available to be picked
+    # if 0, then it is taken or can't be taken
+    prime_game = [1 for x in range(sorted(nums)[-1] + 1)]
+
+    # Mark 0 and 1 as non-prime
+    prime_game[0] = 0
+    prime_game[1] = 0
+
+    # Keep only prime numbers in the list
+    for num in range(2, len(prime_game)):
+        for i in range(2, len(prime_game)):
+            try:
+                prime_game[i * num] = 0
+            except IndexError:
+                break
+
+    # Count the wins, since it is always determined who to start
     for n in nums:
-        primes_in_game = [p for p in primes if p <= n]
-        if not primes_in_game:
+        # If the list is even, ben wins
+        if sum(prime_game[0:n + 1]) % 2 == 0:
             ben_wins += 1
-            continue
 
-        turn = 0
-
-        while primes_in_game:
-            current_prime = primes_in_game[0]
-            primes_in_game = [
-                p for p in primes_in_game if p % current_prime != 0
-                ]
-            turn ^= 1  # Switch turns
-
-        if turn == 1:
-            maria_wins += 1
+        # If the list is odd, maria wins
         else:
-            ben_wins += 1
+            maria_wins += 1
 
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    # Return the winner
+    if ben_wins > maria_wins:
         return "Ben"
-    else:
-        return None
+    if ben_wins < maria_wins:
+        return "Maria"
+    return None
